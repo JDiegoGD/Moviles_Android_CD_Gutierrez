9374ae0fe8c8120c78748d79be7f8518f76f0555
Juan Diego <juan.gutierrez.d@tecsup.edu.pe>
2026-08-26 11:42:36 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana02/R1/app/src/main/java/com/gutierrez/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.gutierrez.lab02carritokotlin
+
+fun main() {
+    println("======================================")
+    println("  CARRITO DE COMPRAS - TIENDA TECSUP  ")
+    println("======================================")
+}
