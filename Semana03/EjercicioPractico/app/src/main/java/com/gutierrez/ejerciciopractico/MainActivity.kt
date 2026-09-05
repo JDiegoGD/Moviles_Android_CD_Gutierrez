515940d1638052dfee24129fbdca49806d970b3b
Juan Diego <juan.gutierrez.d@tecsup.edu.pe>
2026-09-05 13:49:05 -0500

    Agregar Sliders

--- Semana03/EjercicioPractico/app/src/main/java/com/gutierrez/ejerciciopractico/MainActivity.kt
+++ Semana03/EjercicioPractico/app/src/main/java/com/gutierrez/ejerciciopractico/MainActivity.kt
@@ -6,14 +6,31 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -22,15 +39,20 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.gutierrez.ejerciciopractico.ui.theme.EjercicioPracticoTheme
 
+val fondo = Color(0xFFDABED8)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EjercicioPracticoTheme {
-                Column() {
-                    TitleApp()
-                    SubTitleApp()
+            Surface(
+                color = fondo
+            )  {
+                EjercicioPracticoTheme {
+                    Column() {
+                        TitleApp()
+                        SubTitleApp()
+                    }
                 }
             }
         }
@@ -41,7 +63,7 @@ class MainActivity : ComponentActivity() {
 fun TitleApp(){
     Column() {
         Text(
-            text = "Registro de producto",
+            text = "Registro de Notas",
             Modifier
                 .fillMaxWidth()
                 .background(MaterialTheme.colorScheme.primary)
@@ -56,12 +78,17 @@ fun TitleApp(){
 
 @Composable
 fun SubTitleApp(){
+    var nota1 by remember { mutableFloatStateOf(0f) }
+    var nota2 by remember { mutableFloatStateOf(0f) }
+    var nota3 by remember { mutableFloatStateOf(0f) }
+    var nota4 by remember { mutableFloatStateOf(0f) }
+
     Column() {
         Text(
             text = "Notas de ciclo",
             Modifier
                 .fillMaxWidth()
-                .padding(top = 16.dp, bottom = 5.dp, start = 16.dp )
+                .padding(top = 16.dp, start = 16.dp)
             ,
             style = MaterialTheme.typography.headlineSmall,
             fontWeight = FontWeight.Bold
@@ -69,12 +96,110 @@ fun SubTitleApp(){
         Text(
             text = "Desliza para asignar cada nota (0 a 20)",
             Modifier
-                .padding(start = 16.dp),
+                .padding(start = 16.dp, bottom = 5.dp),
             color = Color.Gray,
-            fontSize = 15.sp,
+            fontSize = 14.sp,
         )
+
+        ItemCurso(
+            name = "Fundamentos de Programación",
+            Porcentaje = "(20%)",
+            value = nota1,
+            onValueChange = { nota1 = it }
+        )
+
+        ItemCurso(
+            name = "Programación Orientada a Objetos",
+            Porcentaje = "(25%)",
+            value = nota2,
+            onValueChange = { nota2 = it }
+        )
+
+        ItemCurso(
+            name = "Programación en Móviles",
+            Porcentaje = "(30%)",
+            value = nota3,
+            onValueChange = { nota3 = it }
+        )
+
+        ItemCurso(
+            name = "Bases de Datos",
+            Porcentaje = "(25%)",
+            value = nota4,
+            onValueChange = { nota4 = it }
+        )
+
     }
 }
+
+@Composable
+fun ItemCurso(name: String, Porcentaje: String, value: Float, onValueChange: (Float) -> Unit){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row (
+            modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                Row {
+                    Text(text = name, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                    Spacer(modifier = Modifier.width(4.dp))
+                    val MoradoHeader = null
+
+                    Text(
+                        text = Porcentaje,
+                        fontSize = 16.sp,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+                    Box(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .background(
+                                color = MaterialTheme.colorScheme.primary,
+                                shape = RoundedCornerShape(14.dp)
+                            )
+                            .padding(vertical = 1.dp),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "${value.toInt()}",
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White,
+                            fontSize = 12.sp
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(20.dp))
+                }
+
+                Slider(
+                    value = value,
+                    onValueChange = onValueChange,
+                    valueRange = 0f..20f,
+                    modifier = Modifier.width(300.dp),
+                    colors = SliderDefaults.colors(
+                        thumbColor = MaterialTheme.colorScheme.primary,
+                        activeTrackColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        }
+    }
+}
+
+
+
 
 @Preview(showBackground = true)
 @Composable
--- Semana03/EjercicioPractico/app/src/main/java/com/gutierrez/ejerciciopractico/ui/theme/Color.kt
+++ Semana03/EjercicioPractico/app/src/main/java/com/gutierrez/ejerciciopractico/ui/theme/Color.kt
@@ -3,8 +3,8 @@ package com.gutierrez.ejerciciopractico.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFB821C9)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PurpleGrey80 = Color(0xFF9F8BB2)
+val Pink80 = Color(0xFFE7ABBD)
 
 val Purple40 = Color(0xFF752785)
 val PurpleGrey40 = Color(0xFF625b71)
