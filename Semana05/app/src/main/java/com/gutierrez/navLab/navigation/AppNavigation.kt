348279f2b557a78dcbc30e36ccf7b2dab93656e4
Juan Diego <juan.gutierrez.d@tecsup.edu.pe>
2026-09-22 10:19:46 -0500

    Creación de la sealed class Screen.kt con los objetos: Home, List, Profile, Detail. Integración de Rutas en AppNavigation.kt (Por ahora HomeScreen.kt).

--- Semana05/app/src/main/java/com/gutierrez/navLab/navigation/AppNavigation.kt
+++ Semana05/app/src/main/java/com/gutierrez/navLab/navigation/AppNavigation.kt
@@ -1,2 +1,54 @@
 package com.gutierrez.navLab.navigation
 
+import com.gutierrez.navLab.navigation.Screen
+import androidx.compose.runtime.Composable
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import androidx.navigation.NavType
+import com.gutierrez.navLab.screens.HomeScreen
+
+
+
+@Composable
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ){
+        //Ruta Inicio
+        composable(route = Screen.Home.route){
+            HomeScreen(navController = navController)
+        }
+
+        /*
+        //Ruta List
+        composable(route = Screen.List.route){
+            ListScreen(navController = navController)
+        }
+
+        //Ruta Profile
+        composable(route = Screen.Profile.route){
+            ProfileScreen(navController = navController)
+        }
+
+        //Ruta Detail
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId"){
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ){
+            backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId")?:0
+            DetailScreen(navController, itemId)
+        }*/
+    }
+}
--- Semana05/app/src/main/java/com/gutierrez/navLab/navigation/Screen.kt
+++ Semana05/app/src/main/java/com/gutierrez/navLab/navigation/Screen.kt
@@ -1,4 +1,19 @@
-package com.gutierrez.navLab.navigation;
+package com.gutierrez.navLab.navigation
 
-public class Screen {
+sealed class Screen(val route: String) {
+
+    //Pantalla de inicio - punto de entrada de la app
+    object Home : Screen("home")
+
+    //Pantalla que muestra la lista de elementos
+    object List : Screen("list")
+
+    //Pantalla de perfil de usuario
+    object Profile : Screen("profile")
+
+    //Pantalla de detalle de un elemento - con argumento {itemId}
+    object Detail : Screen("detail/{itemId}"){
+        //Construye la ruta final con el argumento.
+        fun createRoute(itemId: Int) = "detail/$itemId"
+    }
 }
